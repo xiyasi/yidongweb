0bc21080d3afdb29f2d19059823fc6a13651be20
xiyasi <873070555@qq.com>
2019-10-15 09:05:51 +0800

    message

---
+++ build/precache-manifest.384c8c0506e5b96cfee18d328a5c7af3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f4973ffd2652757c4bfe6a7583cec6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5397914236c255135810",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c8c615e779e39dbd4560",
+    "url": "/static/js/2.8f4e7b49.chunk.js"
+  },
+  {
+    "revision": "5397914236c255135810",
+    "url": "/static/js/main.85254d2d.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
---
+++ src/Todolist/Todolist.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Todoing from './Todoing';
+import Todoinput from './Todoinput';
+
+export default class Todolist extends Component {
+    constructor(){
+        super();
+        this.state = {
+            todo: [1,2,3]
+        }
+    }
+    addItem = (msg)=>{
+        // this.state.todo.push(msg)
+        // console.log(this.state.todo)
+        this.setState({
+            todo: [...this.state.todo,msg]
+        })
+        console.log(msg);
+    }
+    delItem = (a)=>{
+        this.state.todo.splice(a,1);
+        this.setState({
+            todo: this.state.todo
+        })
+        console.log(a);
+    }
+    render() {
+        return (
+            <div>
+                <Todoinput addTodo={this.addItem}/>
+                <Todoing delTodo={this.delItem} todo={this.state.todo}/>
+            </div>
+        )
+    }
+}
